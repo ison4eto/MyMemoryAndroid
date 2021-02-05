6872792124c4f895c72e928eea160f98c39107bf
iskra.trifonova <ison4eto635@gmail.com>
2021-02-05 13:54:51 +0200

    Add board size and default icons

--- app/src/main/java/com/iskratrifonova/mymemorygame/MemoryBoardAdapter.kt
+++ app/src/main/java/com/iskratrifonova/mymemorygame/MemoryBoardAdapter.kt
@@ -7,31 +7,35 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.cardview.widget.CardView
-import androidx.core.view.setMargins
 import androidx.recyclerview.widget.RecyclerView
+import com.iskratrifonova.mymemorygame.models.BoardSize
 import kotlin.math.min
 
-class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
+class MemoryBoardAdapter(
+    private val context: Context,
+    private val boardSize: BoardSize,
+    private val cardImages: List<Int>
+) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
     companion object {
-        private const val MARGIN_SIZE = 25
+        private const val MARGIN_SIZE = 10
         private const val TAG = "MemoryBoardAdapter"
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
-        val cardHeight = parent.width / 4 - (2 * MARGIN_SIZE)
+        val cardWidth = parent.width / boardSize.getWidth() - 2 * MARGIN_SIZE
+        val cardHeight = parent.height / boardSize.getHeight() - 2 * MARGIN_SIZE
         val cardSideLength = min(cardWidth, cardHeight)
         val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
         val layoutParams = view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
         layoutParams.width = cardSideLength
         layoutParams.height = cardSideLength
-        layoutParams.setMargins(MARGIN_SIZE)
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
         return ViewHolder(view)
     }
 
-    override fun getItemCount() = numPieces
+    override fun getItemCount() = boardSize.numCards
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(position)
@@ -42,6 +46,8 @@ class MemoryBoardAdapter(private val context: Context, private val numPieces: In
         private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
 
         fun bind(position: Int) {
+            imageButton.setImageResource(cardImages[position
+            ])
             imageButton.setOnClickListener {
                 Log.i(TAG, "Clicked on position $position")
             }
